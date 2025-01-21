50c2dc678908cdbad80dbb8464bd546b5e11a00c
Taaha Mahdi <mahditaaha11@gmail.com>
2025-01-21 00:28:12 -0800

    Move cli setup to another file

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_BATCH_SIZE = 32;
+export const VERSION = "3.1.1";
+export const DEFAULT_TEMPLATED_STRING_PREFIX = "{{";
+export const DEFAULT_TEMPLATED_STRING_SUFFIX = "}}";
+export const FLATTEN_DELIMITER = "*";
