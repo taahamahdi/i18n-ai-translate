8ac45c3f68db24a5c01d7f4dbb6bb2dda4c4e731
Taaha Mahdi <mahditaaha11@gmail.com>
2025-01-26 20:53:56 -0800

    Bump to v3.3.2

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import Engine from "./enums/engine";
 
 export const DEFAULT_BATCH_SIZE = 32;
-export const VERSION = "3.3.1";
+export const VERSION = "3.3.2";
 export const DEFAULT_TEMPLATED_STRING_PREFIX = "{{";
 export const DEFAULT_TEMPLATED_STRING_SUFFIX = "}}";
 export const FLATTEN_DELIMITER = "*";
