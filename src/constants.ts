dc5908eca33deaf0a67de88949d32a22c6ce203e
Taaha Mahdi <mahditaaha11@gmail.com>
2025-07-18 01:11:41 -0700

    Update to v4.1.2

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import Engine from "./enums/engine";
 
 export const DEFAULT_BATCH_SIZE = 32;
 export const DEFAULT_REQUEST_TOKENS = 4096;
-export const VERSION = "4.1.1";
+export const VERSION = "4.1.2";
 export const DEFAULT_TEMPLATED_STRING_PREFIX = "{{";
 export const DEFAULT_TEMPLATED_STRING_SUFFIX = "}}";
 export const FLATTEN_DELIMITER = "*";
